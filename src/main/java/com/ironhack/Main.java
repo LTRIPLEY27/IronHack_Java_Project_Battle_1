5c6d46f9cb715191f9ea1561f649f7ffc13426b4
Alex Arraiza <alexarraiza@users.noreply.github.com>
2022-07-07 21:33:17 +0200

    Uncomment menu on main

--- src/main/java/com/ironhack/Main.java
+++ src/main/java/com/ironhack/Main.java
@@ -8,10 +8,8 @@ import com.ironhack.ui.Menu;
 public class Main {
     public static void main(String[] args) {
 
-//        Menu menu = new Menu();
-//        menu.main();
-
-        Graveyard.test();
+        Menu menu = new Menu();
+        menu.main();
 
     }
 
