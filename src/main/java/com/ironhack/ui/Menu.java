14ac6f0f67a12fee9d6bb09a62b1d6f24a29650f
Alex Arraiza <alexarraiza@gmail.com>
2022-07-12 21:50:42 +0200

    WIP - Menu navigation scaffold - Main Menu - Parties - Graveyard - Battle

--- src/main/java/com/ironhack/ui/Menu.java
+++ src/main/java/com/ironhack/ui/Menu.java
@@ -20,34 +20,76 @@ public class Menu {
             var mainMenu = """
                     Welcome to Battle Simulator
                     ===============
-                    [0] - Warrior creator
-                    [1] - Wizard creator
-                    [2] - Random Warrior
-                    [3] - Random Wizard
-                    [4] - Show Graveyard
-                    [5] - Random Party
-                    [6] - Create Party
-                    [99] - Battle
+                    [0] - PARTY MANAGER
+                    [1] - VIEW GRAVEYARD
+                    [2] - BATTLE
                     
-                    [exit] - Exit Battle Simulator
+                    [EXIT] - Exit Battle Simulator
                     ===============
                     Write your COMMAND:
                     """;
             System.out.println(mainMenu);
             input = scanner.nextLine().trim().toLowerCase();
             switch (input) {
-                case "0" -> createWarrior();
-                case "1" -> createWizard();
-                case "2" -> randomWarrior();
-                case "3" -> randomWizard();
-                case "4" -> showGraveyard();
-                case "5" -> randomParty();
-                case "6" -> createParty();
-                case "99" -> battle();
+                case "0" -> partyManager();
+                case "1" -> graveyardViewer();
+                case "2" -> battle();
                 case "exit" -> printWithColor("Bye bye", ConsoleColors.GREEN);
                 default -> printWithColor("Command not recognized!", ConsoleColors.RED);
             }
         } while (!input.equals("exit"));
+    }
+
+    //region Party
+    private void partyManager() {
+        String input;
+        do {
+            var menu = """
+                    Welcome to Party Manager
+                    ===============
+                    [0] - VIEW PARTIES
+                    [1] - CREATE PARTY
+                    
+                    [BACK] - GO BACK
+                    ===============
+                    Write your COMMAND:
+                    """;
+            System.out.println(menu);
+            input = scanner.nextLine().trim().toLowerCase();
+            switch (input) {
+                case "0" -> partyViewer();
+                case "1" -> createParty();
+                case "back" -> printWithColor("Bye bye", ConsoleColors.GREEN);
+                default -> printWithColor("Command not recognized!", ConsoleColors.RED);
+            }
+        } while (!input.equals("back"));
+    }
+
+    private void createParty() {
+        String input;
+        Party party = new Party();
+        do {
+            var mainMenu = """
+                    Welcome to Party Creator
+                    ===============
+                    [0] - Add Warrior
+                    [1] - Add Wizard
+                    
+                    [BACK] - GO BACK
+                    ===============
+                    Write your COMMAND:
+                    """;
+            System.out.println(mainMenu);
+            input = scanner.nextLine().trim().toLowerCase();
+            switch (input) {
+                case "0" -> party.addCharacter(createWarrior());
+                case "1" -> party.addCharacter(createWizard());
+                case "back" -> printWithColor("Bye bye", ConsoleColors.GREEN);
+                default -> printWithColor("Command not recognized!", ConsoleColors.RED);
+            }
+        } while (!input.equals("back"));
+
+        party.membersParty();
     }
 
     private Wizard createWizard() {
@@ -80,6 +122,68 @@ public class Menu {
         return new Warrior(name, id, hp, stamina, strength);
     }
 
+    private void partyViewer() {
+
+    }
+    //endregion
+
+    //region Graveyard
+
+    private void graveyardViewer() {
+        graveyard.showGraveyard();
+    }
+
+    //endregion
+
+    //region Battle
+    private void battle() {
+
+        String input;
+        do {
+            var menu = """
+                    Welcome to the Battle - Which kind of battle do you want to do?
+                    ===============
+                    [0] - MANUAL BATTLE
+                    [1] - AUTOMATED
+                    
+                    [BACK] - GO BACK
+                    ===============
+                    Write your COMMAND:
+                    """;
+            System.out.println(menu);
+            input = scanner.nextLine().trim().toLowerCase();
+            switch (input) {
+                case "0" -> manualBattle();
+                case "1" -> automatedBattle();
+                case "back" -> printWithColor("Bye bye", ConsoleColors.GREEN);
+                default -> printWithColor("Command not recognized!", ConsoleColors.RED);
+            }
+        } while (!input.equals("back"));
+    }
+
+    private void manualBattle() {
+
+        // TODO: party selection - if not enough parties, create them
+
+        // TODO: select characters that will participate in the battle
+
+        // TODO: Battle.oneVsOneBattle()
+
+        // TODO: remove dead character from party and add it to the graveyard
+
+    }
+
+    private void automatedBattle() {
+
+        // TODO: party selection - if not enough parties, create them
+
+        // TODO: simulate battle - fully automated
+
+    }
+
+
+    //endregion
+
     private void randomWizard() {
         System.out.println(Wizard.generateRandom());
     }
@@ -93,52 +197,6 @@ public class Menu {
         System.out.println("Indique cuántos miembros");
         int num = scanner.nextInt();
         System.out.println(Party.getRandomParty(num));  // --- > VERIFICAR SI DEJAMOS O NO EL MÈTODO COMO ESTÁTICO
-    }
-
-    //SOLO A MODO DE PRUEBA, EDITAR EN LA CLASE GRAVEYARD
-    private void showGraveyard() {
-        Graveyard graveyard = new Graveyard();
-
-        var Merlin = new Wizard("Merlin", "3", 200,  500, 100);
-        var VatoLoco = new Warrior("Vato Loco", "3", 200, 500, 100);
-
-        graveyard.addDeadCharacter(Merlin);
-        graveyard.addDeadCharacter(VatoLoco);
-
-        graveyard.showGraveyard();
-    }
-
-    private void createParty() {
-        String input;
-        Party party = new Party();
-        do {
-            var mainMenu = """
-                    Welcome to Battle Simulator
-                    ===============
-                    [0] - Add Warrior
-                    [1] - Add Wizard
-                    
-                    [exit] - Exit Battle Simulator
-                    ===============
-                    Write your COMMAND:
-                    """;
-            System.out.println(mainMenu);
-            input = scanner.nextLine().trim().toLowerCase();
-            switch (input) {
-                case "0" -> party.addCharacter(createWarrior());
-                case "1" -> party.addCharacter(createWizard());
-                case "exit" -> printWithColor("Bye bye", ConsoleColors.GREEN);
-                default -> printWithColor("Command not recognized!", ConsoleColors.RED);
-            }
-        } while (!input.equals("exit"));
-
-        party.membersParty();
-    }
-
-    private void battle() {
-        Character char1 = new Warrior("First Warrior", Character.generateId(), 200, 30, 10);
-        Character char2 = new Wizard("First Wizard", Character.generateId(), 100,  40, 10);
-        Battle.oneVsOneBattle(char1, char2);
     }
 
     public void printWithColor(String text, String color){
